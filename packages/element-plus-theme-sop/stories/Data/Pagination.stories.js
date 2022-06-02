0881d89f7c7de452112837068e047d7ea98a433a
cuiguojie <cuiguojie@me.com>
2022-06-02 20:44:24 +0800

    refactor: el-pagination

--- packages/element-plus-theme-sop/stories/Data/Pagination.stories.js
+++ packages/element-plus-theme-sop/stories/Data/Pagination.stories.js
@@ -2,6 +2,28 @@ import { ElPagination } from 'element-plus';
 
 export default {
   title: 'Data/Pagination 分页',
+  argTypes: {
+    background: {
+      control: { type: 'boolean' },
+      defaultValue: true,
+      description: '是否使用带有背景色的分页',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: 'true',
+        },
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: '目前设计规范中， 固定使用背景色模式。',
+      },
+    },
+  },
 };
 
 const Template = (args) => ({
@@ -23,7 +45,6 @@ const Template = (args) => ({
       layout="slot, prev, pager, next, jumper, sizes"
       :total="10000"
       :page-sizes="[100, 200, 300, 400]"
-      background
     >
       <span class="el-pagination__text">
         共 100 张专辑，仅显示前 500 页结果
